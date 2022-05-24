a8c913fae97a98ae12eb0203408d474c64ed9f78
Sachin A <sachin@Sachins-MacBook-Pro.local>
2022-05-24 18:59:40 +0530

    Added Confirmation Page

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,18 +1,18 @@
-import { Button } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import React, { useEffect } from 'react'
+import Router from 'next/router'
 
 import Timer from '../../src/components/atoms/Timer/Timer'
 import DatePicker from '../../src/components/molecules/DatePicker/DatePicker'
 import ScreenLayout from '../../src/components/molecules/ScreenLayout/ScreenLayout'
 import styles from '../../styles/movie/[id].module.css'
 import { useStore } from '..'
-import Dialog from "../../src/components/atoms/Dialog/Dialog"
-import { Router } from 'next/router'
+import { Dialog } from "@mui/material"
 
 function BookMovie({ rows = 15, columns = 15, disabledSeats = [{ row: 1, column: 5 }], bookedSeats = [{ row: 2, column: 6 }, { row: 2, column: 7 }] }) {
 
-  const { selectedSeats, setSelectedSeats, bookingTimeExpired } = useStore();
-  const [showPayment, setShowpayment] = React.useState(false);
+  const { setSelectedSeats, selectedSeats, bookingTimeExpired } = useStore();
+  const [showConfirmation, setShowConfirmation] = React.useState(false);
   const [isAlert, setIsAlert] = React.useState(false)
   const layout = {
     columnSplit: [3, 9, 3], rowSplit: [3, 10, 2]
@@ -24,7 +24,7 @@ function BookMovie({ rows = 15, columns = 15, disabledSeats = [{ row: 1, column:
     } else if (bookingTimeExpired) {
       Router.push('/movies')
     } else {
-      setShowpayment(true)
+      setShowConfirmation(true)
     }
   }
 
@@ -33,23 +33,58 @@ function BookMovie({ rows = 15, columns = 15, disabledSeats = [{ row: 1, column:
   }
 
   const closePayment = () => {
-    setShowpayment(false)
+    Router.push("/movies")
   }
 
   useEffect(() => {
     return () => {
-      setShowpayment(false)
+      setShowConfirmation(false)
       setIsAlert(false)
+      setSelectedSeats([])
     }
   }, [])
+
+  const ShowPayment = () => {
+    return (<>
+      <Dialog open={showConfirmation} onClose={closePayment}>
+        <div className={styles.payment}>
+          <div className={styles.paymentHeading}>
+            <Typography variant='h2'>Payment</Typography>
+          </div>
+          <div >
+            <Typography variant='h6'>Total Amount : <span>Rs. {selectedSeats.length * 100}</span></Typography>
+            <Typography variant='h6'>Payment Mode : <span>Online</span></Typography>
+            <Typography variant='h6'>Payment Status : <span>Success</span></Typography>
+
+            <hr />
+            <div className={styles.bookedSeats}>
+              <Typography variant='h4'>Booked Seats </Typography>
+              {selectedSeats.map(seat => {
+                return <Typography key={`${seat.row}-${seat.column}`} variant='h6'>Row: {seat.row} - Column: {seat.column}</Typography>
+              })}
+            </div>
+          </div>
+          <div className={styles.paymentFooter}>
+            <Button variant='contained' color='primary' onClick={closePayment}>Close</Button>
+          </div>
+        </div>
+      </Dialog>
+    </>)
+  }
 
   return (
     <>
       <div className={styles.bookingContainer}>
-        {showPayment ? <p>Payment</p> :
+        {showConfirmation ?
+          <>
+            <p>Enjoy the Movie!</p>
+            <ShowPayment />
+          </> :
           <>
             <DatePicker />
             <Timer />
+            <p> Screen this way!</p>
+            <hr style={{ width: "100%" }} />
             <div className={styles.screen}>
               <ScreenLayout bookedSeats={bookedSeats} rows={rows} columns={columns} disabledSeats={disabledSeats} layout={layout} />
             </div>
