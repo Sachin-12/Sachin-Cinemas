cb0ebcc13597a524743fec81a2dc9cfc71af68b1
Sachin A <sachin@Sachins-MacBook-Pro.local>
2022-05-26 12:46:34 +0530

    added pagination

--- pages/movies.js
+++ pages/movies.js
@@ -6,21 +6,28 @@ import { useStore } from './index';
 import styles from '../styles/movies.module.css'
 import MovieCard from '../src/components/atoms/MovieCard/MovieCard';
 import getMovies from '../src/utility/getMovies';
+import { Pagination } from '@mui/material';
 
 function Movies(props) {
-  const { page, limit, setSelectedMovieId, } = useStore()
+  const { page, limit, setSelectedMovieId, setPage } = useStore()
 
   const { data: moviesData, isLoading, isError } = useQuery(['movies', page, limit], () => getMovies(page, limit), {
     initialData: props.movies
   })
+
 
   const handleClick = (movieId) => {
     setSelectedMovieId(movieId)
     Router.push(`/movie/${movieId}`)
   }
 
+
   return (
     <>
+    <Pagination className={styles.pagination} count={moviesData?.total_pages}page={page} limit={limit} onChange={(e, page)=>{
+      setPage(page)
+    }} />
+      
       <div className={styles.movieContainer}>
         {moviesData?.results.map(movie => (
           <div className={styles.movieCard} key={movie.uuid} >
